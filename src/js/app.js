ef6425e9e5e01d00a42ab1d725c94d62ac5f692e
Mauricio Ivan Mejia Ramirez <mejiarmairez@outlook.com>
2020-08-31 18:19:31 -0500

    Update Site

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,44 @@
+function success() {
+    alert("Success")
+    alertify.logPosition("bottom right");
+    alertify.success("Correo enviado, gracias por contactarte con nosotros.");
+    $('form[name="promor-contact-form"]')[0].reset();
+}
+
+function error() {
+    alert("Error")
+}
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
+
 $('form[name="promor-contact-form"]').validate({
-  name: 'required',
-  email: 'required',
-  messages: {
-   name: "Por favor, introduce tu Nombre.",
-   email: "Por favor, introduce un Correo Electrónico."
-  },
-  submitHandler: function(form) {
-   var data = $('form[name="promor-contact-form"]').serialize();
-   console.log(data);
-   $.ajax({
-     url: 'http://integrations.blick.mx/promor/contact/',
-     method: 'POST',
-     data: data
-   }).done(function(data) {
-     if (parseInt(data) === 1) {
-       alertify.logPosition("bottom right");
-       alertify.success("Correo enviado, gracias por contactarte con nosotros.");
-       $('form[name="promor-contact-form"]')[0].reset();
-     }
-   });
-  },
-  invalidHandler: function(event, validator) {
-   var errors = validator.numberOfInvalids();
-   alertify.logPosition("bottom right");
-   alertify.error("Verifica tu información tienes " + errors + " errores.");
-  }
+    name: 'required',
+    email: 'required',
+    messages: {
+        name: "Por favor, introduce tu Nombre.",
+        email: "Por favor, introduce un Correo Electrónico."
+    },
+    submitHandler: function (form) {
+        var form = document.getElementById("contact-form");
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    },
+    invalidHandler: function (event, validator) {
+        var errors = validator.numberOfInvalids();
+        alertify.logPosition("bottom right");
+        alertify.error("Verifica tu información tienes " + errors + " errores.");
+    }
 });
